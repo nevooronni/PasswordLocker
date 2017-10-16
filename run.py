cb6d16a4a6b401de87573b71276413b21fad2249
Neville Oronni <nevooronni@gmail.com>
2017-10-16 16:37:38 +0300

    added call functions for all the credential behaviours

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.6 
 
 from accounts import User
+from accounts import Credentials 
 
 #creating a new user account
 def create_account(first_name,last_name,email,password):
@@ -11,6 +12,7 @@ def create_account(first_name,last_name,email,password):
 				new_user = User(first_name,last_name,email,password)
 				return new_user
 
+#saving a new user account
 def save_account(user):
 				"""
 				function to save new user account
@@ -18,18 +20,21 @@ def save_account(user):
 
 				user.save_user()
 
+#dele a user account
 def delete_account(user):
 				"""
 				function to delete a user account
 				"""
 				user.delete_user()
 
+#login into a user account
 def login_user(email,password):
 				"""
 				function that finds a user accoutn and logs user in 
 				"""
 				return User.find_by(email,password)
 
+#confirm if a user account exists
 def check_users(email,password):
 				"""
 				function that checks to see if a user exists in the list befor calling login_user to 
@@ -37,14 +42,54 @@ def check_users(email,password):
 				"""
 				return User.user_exist(email,password)
 
+
+#creates a new credential
+def create_credential(account_name,description,password):
+				"""
+				function to create new credential
+				"""
+				new_credential = Credentials(account_name,description,password)
+				return new_credential
+
+#function that save user credentials
+def save_credential(credential):
+				"""
+				save credential
+				"""
+				credential.save_credential()
+
+def delete_credential(credential):
+				"""
+				deletes a credential
+				"""
+				credential.delete_credential()_
+
+def get_credential(name):
+				"""
+				finds a credential for the user
+				"""
+				return Credentials.find_by_name(name)
+
+def check_credential(name):
+				"""
+				checks to find if a credential exists and returns a true/false value
+				"""
+				return Credentials.credential_check(name)
+
+def list_credentials():
+				"""
+				returns all of the saved credentials
+				"""
+				return Credentials.show_credentials()
+
 #task function
 def main():
 				while True:
-						print("Welcome to password Locker! the best website to store all of your important acount details use cn - create new account/sign up or l - login to an existing account or x - exit the application")		
+						print("Welcome to password Locker! the best website to store all of your important acount details use CN - create new account/sign up or L - login to an existing account or X - exit the application")		
 						print("\n")
 						code = input().lower()
 
-						if code == 'cn':
+						if code == 'CN':
 								print("New Account")
 								print("\n")
 
@@ -70,7 +115,7 @@ def main():
 								print(f"Created account for {first_name} {last_name} with email {email}")
 								print("\n")
 
-						elif code =='l':
+						elif code =='L':
 								print("Enter an email address for your user account")
 								search_email = input()
 
@@ -84,12 +129,15 @@ def main():
 										print("\n")
 										print(f"UserName: {search_account.first_name} {search_account.last_name}")
 										print(f"Email Address: {search_account.email}")
+										print("\n")
+										print("Press V to view a list of your credentials or N")
+
 
 								else:
 										print("That user account does not exist please sign up t create a new account!")
 										print("\n")		
 
-						elif code == 'x':
+						elif code == 'X':
 								print("Nooooooo....don't go!")
 								break
 						else:
